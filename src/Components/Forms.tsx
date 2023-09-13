482dae3d325f4207b627911903ba453382da7864
Stephan <cicchelli.stephan@gmail.com>
2023-09-13 19:34:18 -0300

    req6

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -1,8 +1,122 @@
+// import React, { useContext, useState } from 'react';
+// import GlobalContext from '../context/GlobalContext';
+// import { DataFiltersType } from '../Types';
+
+// const arrayOptions = ['population', 'orbital_period', 'diameter',
+//   'rotation_period', 'surface_water'];
+
+// const dataFilters = {
+//   column: arrayOptions[0],
+//   filter: 'maior que',
+//   value: 0,
+// };
+// export default function Forms() {
+//   const { filterResultsByText, filterResultsByValue } = useContext(GlobalContext);
+//   const [search, setSearch] = useState('');
+//   const [choiceFilters, setChoiceFilters] = useState<DataFiltersType>(dataFilters);
+//   const [multipleFilters, setMultipleFilters] = useState<DataFiltersType[]>([]);
+//   const [showFilter, setShowFilter] = useState(false);
+//   const [options, setOptions] = useState(arrayOptions);
+
+//   function handleChange({ target }: React.ChangeEvent<HTMLInputElement>) {
+//     setSearch(target.value);
+//     filterResultsByText(target.value);
+//   }
+//   function handleFilters({ target }: React.ChangeEvent<HTMLInputElement |
+//   HTMLSelectElement>) {
+//     const { name, value } = target;
+//     setChoiceFilters({
+//       ...choiceFilters,
+//       [name]: value,
+//     });
+//   }
+//   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+//     event.preventDefault();
+//     setShowFilter(true);
+//     filterResultsByValue(choiceFilters);
+//     setMultipleFilters([
+//       ...multipleFilters,
+//       choiceFilters,
+//     ]);
+//     hiddenOptions(choiceFilters.column);
+//   }
+
+//   function hiddenOptions(value: string) {
+//     if (choiceFilters.column.length && choiceFilters.column === value) {
+//       const newOptions = options.filter((op) => op !== value);
+//       setOptions(newOptions);
+//     }
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={ handleSubmit }>
+//         <input
+//           data-testid="name-filter"
+//           type="text"
+//           value={ search }
+//           onChange={ handleChange }
+//         />
+//         <select
+//           data-testid="column-filter"
+//           defaultValue={ choiceFilters.column }
+//           onChange={ handleFilters }
+//           name="column"
+//         >
+//           {options.map((option, i) => (
+//             <option
+//               key={ i }
+//               value={ option }
+//             >
+//               {option}
+//             </option>
+//           ))}
+//         </select>
+//         <select
+//           data-testid="comparison-filter"
+//           defaultValue={ choiceFilters.filter }
+//           onChange={ handleFilters }
+//           name="filter"
+//         >
+//           <option value="maior que">maior que</option>
+//           <option value="menor que">menor que</option>
+//           <option value="igual a">igual a</option>
+//         </select>
+//         <input
+//           type="number"
+//           data-testid="value-filter"
+//           name="value"
+//           defaultValue={ choiceFilters.value }
+//           onChange={ handleFilters }
+//         />
+//         <button data-testid="button-filter">Filter</button>
+//       </form>
+//       {showFilter && (
+//         multipleFilters.map((filter, i) => (
+//           <div key={ i }>
+//             <p>
+//               {filter.column}
+//               {' '}
+//               {filter.filter}
+//               {' '}
+//               {filter.value}
+//               <button>X</button>
+//             </p>
+//           </div>
+//         ))
+//       )}
+//     </>
+//   );
+// }
 import React, { useContext, useState } from 'react';
 import GlobalContext from '../context/GlobalContext';
+import { DataFiltersType } from '../Types';
+
+const arrayOptions = [
+  'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
 
 const defaultFilters = {
-  column: 'population',
+  column: arrayOptions[0],
   filter: 'maior que',
   value: 0,
 };
@@ -10,27 +124,30 @@ const defaultFilters = {
 export default function Forms() {
   const { filterResultsByText, filterResultsByValue } = useContext(GlobalContext);
   const [search, setSearch] = useState('');
-  const [filters, setFilters] = useState(defaultFilters);
+  const [choice, setChoice] = useState<DataFiltersType>(defaultFilters);
+  const [multipleFilters, setMultipleFilters] = useState<DataFiltersType[]>([]);
   const [showFilter, setShowFilter] = useState(false);
+  const [options, setOptions] = useState(arrayOptions);
 
-  const handleChange = (event: { target: { value: any; }; }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setSearch(value);
     filterResultsByText(value);
   };
 
-  const handleFiltersChange = (event: { target: { name: any; value: any; }; }) => {
+  const handleFiltersChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
     const { name, value } = event.target;
-    setFilters({
-      ...filters,
-      [name]: value,
-    });
+    setChoice((prevFilters) => ({ ...prevFilters, [name]: value }));
   };
 
-  const handleSubmit = (event: { preventDefault: () => void; }) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setShowFilter(true);
-    filterResultsByValue(filters);
+    filterResultsByValue(choice);
+    setMultipleFilters((prevFilters) => [...prevFilters, choice]);
+    setOptions((prevOptions) => prevOptions.filter((option) => option !== choice.column));
   };
 
   return (
@@ -44,19 +161,19 @@ export default function Forms() {
         />
         <select
           data-testid="column-filter"
-          value={ filters.column }
+          value={ choice.column }
           onChange={ handleFiltersChange }
           name="column"
         >
-          <option value="population">population</option>
-          <option value="orbital_period">orbital_period</option>
-          <option value="diameter">diameter</option>
-          <option value="rotation_period">rotation_period</option>
-          <option value="surface_water">surface_water</option>
+          {options.map((option, i) => (
+            <option key={ i } value={ option }>
+              {option}
+            </option>
+          ))}
         </select>
         <select
           data-testid="comparison-filter"
-          value={ filters.filter }
+          value={ choice.filter }
           onChange={ handleFiltersChange }
           name="filter"
         >
@@ -68,19 +185,25 @@ export default function Forms() {
           type="number"
           data-testid="value-filter"
           name="value"
-          value={ filters.value }
+          value={ choice.value }
           onChange={ handleFiltersChange }
         />
         <button data-testid="button-filter">Filter</button>
       </form>
       {showFilter && (
-        <p>
-          {filters.column}
-          {' '}
-          {filters.filter}
-          {' '}
-          {filters.value}
-        </p>
+        <div>
+          {multipleFilters.map((filter, i) => (
+            <p key={ i }>
+              {filter.column}
+              {' '}
+              {filter.filter}
+              {' '}
+              {filter.value}
+              {' '}
+              <button>X</button>
+            </p>
+          ))}
+        </div>
       )}
     </>
   );
